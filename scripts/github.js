45ad982c73bc74aac2443b0ad204c5aa5302fcbe
Min <the7mincheol@gmail.com>
2025-01-16 22:54:09 +0900

    feat: github shortcut update for issue page DOM update

--- scripts/github.js
+++ scripts/github.js
@@ -13,7 +13,8 @@
   /* shortcut                                       */
   /*                                                */
   /**************************************************/
-  addCommentShortcuts();
+  addCommentShortcutsInIssue();
+  addCommentShortcutsInPr();
 
   addPrCommitLinksAutoEmbed();
 })();
@@ -187,7 +188,89 @@ async function addNotificationFilters() {
   });
 }
 
-function addCommentShortcuts() {
+function addCommentShortcutsInIssue() {
+  let currentComment = null;
+  const timeout = 10_000;
+
+  const threeDotButtonSelector =
+    '[data-component="IconButton"][aria-haspopup]:has(.octicon-kebab-horizontal)';
+  const commentSelector = `[data-testid="issue-body"], [data-timeline-event-id]`;
+  const overlayContainerSelector = "#__primerPortalRoot__";
+  const editButtonSelector = '[aria-keyshortcuts="e"]';
+  const cancelButtonSelector = 'button[value="Cancel"]';
+
+  const COMMANDS = {
+    cmdShiftE: {
+      keyPressed: (e) =>
+        (e.metaKey || e.ctrlKey) && e.shiftKey && e.key.toLowerCase() === "e",
+      handler: (currentComment) => {
+        const threeDotButton = currentComment.querySelector(
+          threeDotButtonSelector
+        );
+        if (!threeDotButton) return;
+
+        threeDotButton.click();
+        const targetSelector = editButtonSelector;
+
+        waitForElement(
+          document.querySelector(overlayContainerSelector),
+          targetSelector,
+          timeout
+        ).then((element) => {
+          element.click();
+        });
+      },
+    },
+    esc: {
+      keyPressed: (e) => e.key.toLowerCase() === "escape",
+      handler: (currentComment) => {
+        const cancelButton = currentComment.querySelector(cancelButtonSelector);
+        if (!cancelButton) return;
+
+        cancelButton.click();
+      },
+    },
+  };
+
+  document.addEventListener("click", (e) => {
+    const comment = e.target.closest(commentSelector);
+    if (!comment) return;
+
+    currentComment = comment;
+    glow(currentComment);
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (!currentComment) return;
+
+    const command = Object.entries(COMMANDS).find(([_, cmd]) =>
+      cmd.keyPressed(e)
+    );
+    if (!command) return;
+
+    const [_, { handler }] = command;
+    handler(currentComment);
+  });
+
+  function glow(element) {
+    setTimeout(() => {
+      element.style.transition = "box-shadow 0.3s ease-in-out";
+      element.style.boxShadow = "0 0 8px 2px blueviolet";
+
+      setTimeout(() => {
+        element.style.boxShadow = "0 0 0 0 blueviolet";
+
+        // Clean up styles after animation
+        setTimeout(() => {
+          element.style.transition = "";
+          element.style.boxShadow = "";
+        }, 300);
+      }, 900);
+    }, 0);
+  }
+}
+
+function addCommentShortcutsInPr() {
   let currentComment = null;
   const timeout = 10_000;
 
