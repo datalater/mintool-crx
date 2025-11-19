d5e92bbff891edb08bb4b68ddd4b055d63001802
Min <the7mincheol@gmail.com>
2025-11-19 22:59:07 +0900

    feat: add ad-block keyword

--- scripts/ad-block.js
+++ scripts/ad-block.js
@@ -9,8 +9,19 @@ const BLOCKED_LIST = [
       ".google-auto-placed", // tistory.com
       ".revenue_unit_wrap", // tistory.com
       ".adsbygoogle", // tistory.com
+      ".kakao_ad_area", // kakao_ad_area
+      '[id*="criteo"]', // criteo ads
       // "[data-gg-moat]", // diffchecker.com
       // "[class*='ad-google']", // diffchecker.com
+      "[data-google-query-id]", // general google ads
+      "script[src*='ezad']", // ezad script
+      "script[src*='ezoic']", // ezad script
+      "script[src*='ezodn']", // ezad script
+      "script[src*='ads']", // general ads script
+      "script[src*='anyflip']", // general ads script
+      "iframe[src*='ads']", // general ads iframe
+      "iframe[src*='ezoic']", // general ads iframe
+      "[id*='ezoic']", // general ads iframe
     ],
     origin: "",
     command: COMMAND.remove,
