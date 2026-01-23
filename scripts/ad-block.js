bd52e28405592fca6774f756af3747370588b3ad
Min <the7mincheol@gmail.com>
2026-01-23 16:07:59 +0900

    feat: improve ad-block perf and injectStylesheet for tanstack ad
    performance:
    - observer runs execute function only on script insertion

--- scripts/ad-block.js
+++ scripts/ad-block.js
@@ -1,26 +1,36 @@
 const COMMAND = Object.freeze({
-  remove: "remove",
-  "display: none": "display: none",
+  remove: (element) => {
+    element.remove();
+  },
+  displayNone: (element) => {
+    element.style.display = "none";
+  },
+  visibilityHidden: (element) => {
+    element.style.visibility = "hidden";
+  },
+  injectStyle: (styleText) => (element) => {
+    injectStylesheetForAdblock(styleText);
+  },
 });
 
 const BLOCKED_LIST = [
   {
     selectors: [
-      ".google-auto-placed", // tistory.com
-      ".revenue_unit_wrap", // tistory.com
-      ".adsbygoogle", // tistory.com
-      ".kakao_ad_area", // kakao_ad_area
-      '[id*="criteo"]', // criteo ads
+      // ".google-auto-placed", // tistory.com
+      // ".revenue_unit_wrap", // tistory.com
+      // ".adsbygoogle", // tistory.com
+      // ".kakao_ad_area", // kakao_ad_area
+      // '[id*="criteo"]', // criteo ads
       // "[data-gg-moat]", // diffchecker.com
       // "[class*='ad-google']", // diffchecker.com
       // "script[src*='ads']", // general ads script
       // "iframe[src*='ads']", // general ads iframe
       // "[data-google-query-id]", // general google ads
-      "script[src*='ezad']", // ezad script
-      "script[src*='ezoic']", // ezad script
-      "script[src*='ezodn']", // ezad script
-      "iframe[src*='ezoic']", // general ads iframe
-      "[id*='ezoic']", // general ads iframe
+      // "script[src*='ezad']", // ezad script
+      // "script[src*='ezoic']", // ezad script
+      // "script[src*='ezodn']", // ezad script
+      // "iframe[src*='ezoic']", // general ads iframe
+      // "[id*='ezoic']", // general ads iframe
     ],
     origin: "",
     command: COMMAND.remove,
@@ -28,23 +38,22 @@ const BLOCKED_LIST = [
   {
     selectors: [".spacing_log_question_page_ad"],
     origin: "https://www.quora.com",
-    command: COMMAND["display: none"],
+    command: COMMAND.displayNone,
   },
   {
     selectors: [".ceriad", '[id*="google_ads_iframe"]'],
     origin: "https://tetr.io",
     command: COMMAND.remove,
   },
+  {
+    selectors: [".fuse-slot"],
+    origin: "https://tanstack.com",
+    command: COMMAND.injectStyle(`
+      .fuse-slot {
+        display: none !important;
+      `),
+  },
 ];
-
-const COMMANDS = {
-  [COMMAND["display: none"]]: (element) => {
-    element.style.display = "none";
-  },
-  [COMMAND.remove]: (element) => {
-    element.remove();
-  },
-};
 
 (function main() {
   if (!document.body) return;
@@ -52,11 +61,16 @@ const COMMANDS = {
   execute();
 
   const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.type === "childList") {
-        execute();
+    for (const mutation of mutations) {
+      if (mutation.type !== "childList") continue;
+
+      for (const node of mutation.addedNodes) {
+        if (node.nodeType === Node.ELEMENT_NODE && node.tagName === "SCRIPT") {
+          execute();
+          return;
+        }
       }
-    });
+    }
   });
 
   observer.observe(document.body, {
@@ -73,12 +87,25 @@ function execute() {
 
     blocked.selectors.forEach((selector) => {
       document.querySelectorAll(selector).forEach((element) => {
-        COMMANDS[blocked.command](element);
-        showToast(
-          `[MINTOOL] "${blocked.command}" is executed on "${selector}"`,
-          3_000
-        );
+        blocked.command(element);
+
+        const message = `[MINTOOL] ad-block "${blocked.command}" is executed on "${selector}"`;
+
+        showToast(message, 3_000);
+        console.log(`%c${message}`, `color: #ffa07a`);
       });
     });
   });
+}
+
+function injectStylesheetForAdblock(text) {
+  const uniqueId = `mintool-adblock-${JSON.stringify(text.trim().replace(/\s+/g, ""))}`;
+  if (document.getElementById(uniqueId)) {
+    return;
+  }
+
+  const style = document.createElement("style");
+  style.id = uniqueId;
+  style.textContent = text;
+  document.head.appendChild(style);
 }
